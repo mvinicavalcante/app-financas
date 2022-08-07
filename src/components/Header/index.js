429d7e9205dac96581f8bcf1c8ba196f0c22cfc4
Marcos Vinícius <mvinic2011@gmail.com>
2022-08-07 17:02:28 -0300

    Finalização visual

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,19 +8,50 @@ import{
 } from 'react-native'
 
 import { Feather } from '@expo/vector-icons'
+import { MotiView, MotiText } from 'moti'
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
 
-export default function Header() {
+export default function Header({ name }) {
     return(
         <View style={styles.container}>
-            <View style={styles.content}>
-                <Text style={styles.username}>Sujeito Programador</Text>
+            <MotiView 
+                style={styles.content}
+                from={{
+                    translateY: -150,
+                    opacity: 0,
+                }}
+                animate={{
+                    translateY: 0,
+                    opacity: 1,
+                }}
+                transition={{
+                    type: "timing",
+                    duration: 800,
+                    delay: 300
+                }}
+            >
+                <MotiText 
+                    style={styles.username}
+                    from={{
+                        translateX: -300
+                    }}
+                    animate={{
+                        translateX: 0
+                    }}
+                    transition={{
+                        type: 'timing',
+                        duration: 800,
+                        delay: 800
+                    }}
+                >
+                    {name}
+                </MotiText>
 
                 <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
                     <Feather name="user" size={27} color="#FFF"/>
                 </TouchableOpacity>
-            </View>
+            </MotiView>
 
         </View>
     )
@@ -41,6 +72,21 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'row',
         justifyContent: 'space-between'
+    },
+
+    username: {
+        fontSize: 18,
+        color: '#FFF',
+        fontWeight: 'bold'
+    },
+
+    buttonUser: {
+        width: 44,
+        height: 44,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 44 / 2,
     }
 
 })
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+import Actions from '../../components/Actions';
+
+const list = [
+  {
+    id: 1,
+    label: 'Netflix',
+    value: '55,00',
+    date: '8/08/2022',
+    type: 0 //despesas
+  },
+  {
+    id: 2,
+    label: 'Pix de cliente',
+    value: '2.500,00',
+    date: '28/07/2022',
+    type: 1 //receita
+  },
+  {
+    id: 3,
+    label: 'Salário',
+    value: '5.000,00',
+    date: '10/07/2022',
+    type: 1 //receita
+  },
+]
 
 export default function Home() {
+
   return (
     <View style={styles.container}>
-        <Header />
-      <Text>Página Home</Text>
+        <Header name="Marcos Vinícius" />
+        <Balance saldo="15.550,40" gastos="7.990,95"/>
+
+        <Actions />
+
+        <Text style={styles.title}>Últimas movimentações</Text>
+        
+        <FlatList 
+          style={styles.list}
+          data={list}
+          keyExtractor={ (item) => String(item.id)}
+          showsVerticalScrollIndicator={false}
+          renderItem={ ({ item }) => <Movements data={item} />}
+        />
+        
     </View>
   );
 }
@@ -16,6 +57,18 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
   },
+
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 14
+  },
+  
+  list: {
+    marginStart: 14,
+    marginEnd: 14,
+  },
+  
 });
